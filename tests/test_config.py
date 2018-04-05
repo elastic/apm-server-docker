615930d51acb03361a88dd0cba865d8e93694232
Silvia Mitter <silvia.mitter@elastic.co>
2018-04-05 09:12:01 +0200

    Use default apm-server.yml, but override elasticsearch output. (#12)
    Use config file from downloaded tar.
    
    Override Elasticsearch output config to `elasticsearch:9200`
    to avoid breaking change.
    
    closes elastic/apm-server#776

--- tests/test_config.py
+++ tests/test_config.py
@@ -4,3 +4,8 @@ from .fixtures import apm_server
 def test_config_file_passes_config_test(Command, apm_server):
     configtest = '%s -c %s -configtest' % (apm_server.binary_file.path, apm_server.config_file.path)
     Command.run_expect([0], configtest)
+
+
+def test_elasticsearch_output_points_to_elasticsearch_host(apm_server):
+    configured_hosts = apm_server.config['output']['elasticsearch']['hosts']
+    assert configured_hosts == ['elasticsearch:9200']
