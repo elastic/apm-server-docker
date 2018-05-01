2f342930f0c044551012dfe7303be99f3449c830
Gil Raphaelli <g@raphaelli.com>
2018-05-01 09:38:55 -0400

    make listener useful by default
    when running under docker using bridge networking

--- tests/test_config.py
+++ tests/test_config.py
@@ -6,6 +6,11 @@ def test_config_file_passes_config_test(Command, apm_server):
     Command.run_expect([0], configtest)
 
 
+def test_apm_server_listens_on_all_interfaces(apm_server):
+    configured_host = apm_server.config['apm-server']['host']
+    assert configured_host == '0.0.0.0:8200'
+
+
 def test_elasticsearch_output_points_to_elasticsearch_host(apm_server):
     configured_hosts = apm_server.config['output']['elasticsearch']['hosts']
     assert configured_hosts == ['elasticsearch:9200']
